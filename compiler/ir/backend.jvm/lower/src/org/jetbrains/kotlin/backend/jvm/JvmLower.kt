40c191e10b0a43f41a26859de459e88fe271b8d5
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2022-08-12 00:51:46 +0300

    Improve ir inliner to inline property references

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/JvmLower.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/JvmLower.kt
@@ -337,13 +337,6 @@ private val functionInliningPhase = makeIrModulePhase<JvmBackendContext>(
     )
 )
 
-internal val propertyReferenceInliningPhase = makeIrModulePhase(
-    ::PropertyReferenceInliningLowering,
-    name = "PropertyReferenceInliningPhase",
-    description = "Replace `invoke` call of KProperty by explicit call to getter",
-    prerequisite = setOf(functionInliningPhase)
-)
-
 private val jvmFilePhases = listOf(
     kCallableNamePropertyPhase,
     annotationPhase,
@@ -486,7 +479,6 @@ private fun buildJvmLoweringPhases(
 
                 functionInliningPhase then
                 inventNamesForNewLocalClassesPhase then
-                propertyReferenceInliningPhase then
                 provisionalFunctionExpressionPhase then
 //            inventNamesForLocalClassesPhase2 then
 //            sharedVariablesPhase then
