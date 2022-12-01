80ace7c61d4ca0af35ec412f6a7faca6582a209e
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-12-01 14:07:28 +0100

    FE: move delegate name functions to NameUtils

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -37,6 +37,7 @@ import org.jetbrains.kotlin.fir.types.impl.FirTypeArgumentListImpl
 import org.jetbrains.kotlin.lexer.KtTokens.*
 import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.NameUtils
 import org.jetbrains.kotlin.name.SpecialNames
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.*
@@ -855,7 +856,7 @@ open class RawFirBuilder(
                             source = delegateSource
                             moduleData = baseModuleData
                             origin = FirDeclarationOrigin.Synthetic
-                            name = SpecialNames.delegateFieldName(delegateFieldsMap.size)
+                            name = NameUtils.delegateFieldName(delegateFieldsMap.size)
                             returnTypeRef = type
                             symbol = FirFieldSymbol(CallableId(name))
                             isVar = false
