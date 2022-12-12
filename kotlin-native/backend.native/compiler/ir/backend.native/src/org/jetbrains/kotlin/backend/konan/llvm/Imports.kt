TechMarinar/kotlinLanguage

[K/N][IR][codegen] Build per-file dependencies for caches

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/Imports.kt
@@ -8,16 +8,21 @@ package org.jetbrains.kotlin.backend.konan.llvm
 import org.jetbrains.kotlin.backend.konan.Context
 import org.jetbrains.kotlin.backend.konan.descriptors.isExpectMember
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.konan.CompiledKlibFileOrigin
 import org.jetbrains.kotlin.descriptors.konan.CompiledKlibModuleOrigin
 import org.jetbrains.kotlin.descriptors.konan.SyntheticModulesOrigin
 import org.jetbrains.kotlin.descriptors.konan.klibModuleOrigin
 import org.jetbrains.kotlin.konan.library.KonanLibrary
+import org.jetbrains.kotlin.library.KotlinLibrary
 import org.jetbrains.kotlin.resolve.descriptorUtil.module
 
 internal interface LlvmImports {
-    fun add(origin: CompiledKlibModuleOrigin, onlyBitcode: Boolean = false)
+    data class LibraryFile(val library: KotlinLibrary, val fqName: String, val filePath: String)
+
+    fun add(origin: CompiledKlibModuleOrigin, fileOrigin: CompiledKlibFileOrigin, onlyBitcode: Boolean = false)
     fun bitcodeIsUsed(library: KonanLibrary): Boolean
     fun nativeDependenciesAreUsed(library: KonanLibrary): Boolean
+    fun usedBitcode(): List<LibraryFile>
 }
 
 internal val DeclarationDescriptor.llvmSymbolOrigin: CompiledKlibModuleOrigin

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/LlvmFunctionPrototype.kt
@@ -12,6 +12,7 @@ import llvm.*
 import org.jetbrains.kotlin.backend.konan.Context
 import org.jetbrains.kotlin.backend.konan.RuntimeNames
 import org.jetbrains.kotlin.backend.konan.ir.llvmSymbolOrigin
+import org.jetbrains.kotlin.descriptors.konan.CompiledKlibFileOrigin
 import org.jetbrains.kotlin.descriptors.konan.CompiledKlibModuleOrigin
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
 import org.jetbrains.kotlin.ir.declarations.IrFunction
@@ -157,6 +158,7 @@ internal class LlvmFunctionProto(
         parameterTypes: List<LlvmParamType> = emptyList(),
         functionAttributes: List<LlvmFunctionAttribute> = emptyList(),
         val origin: CompiledKlibModuleOrigin,
+        val fileOrigin: CompiledKlibFileOrigin,
         isVararg: Boolean = false,
         val independent: Boolean = false,
 ) : LlvmFunctionSignature(returnType, parameterTypes, isVararg, functionAttributes) {
@@ -164,8 +166,9 @@ internal class LlvmFunctionProto(
             name: String,
             signature: LlvmFunctionSignature,
             origin: CompiledKlibModuleOrigin,
+            fileOrigin: CompiledKlibFileOrigin,
             independent: Boolean = false,
-    ) : this(name, signature.returnType, signature.parameterTypes, signature.functionAttributes, origin, signature.isVararg, independent)
+    ) : this(name, signature.returnType, signature.parameterTypes, signature.functionAttributes, origin, fileOrigin, signature.isVararg, independent)
 
     constructor(irFunction: IrFunction, symbolName: String, contextUtils: ContextUtils) : this(
             name = symbolName,
@@ -173,6 +176,7 @@ internal class LlvmFunctionProto(
             parameterTypes = contextUtils.getLlvmFunctionParameterTypes(irFunction),
             functionAttributes = inferFunctionAttributes(contextUtils, irFunction),
             origin = irFunction.llvmSymbolOrigin,
+            fileOrigin = contextUtils.context.irLinker.getFileOrigin(irFunction),
             independent = irFunction.hasAnnotation(RuntimeNames.independent)
     )
 }

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/objcexport/BlockPointerSupport.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.backend.konan.llvm.objcexport
 import llvm.*
 import org.jetbrains.kotlin.backend.konan.llvm.*
 import org.jetbrains.kotlin.backend.konan.objcexport.BlockPointerBridge
+import org.jetbrains.kotlin.descriptors.konan.CompiledKlibFileOrigin
 import org.jetbrains.kotlin.descriptors.konan.CurrentKlibModuleOrigin
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.util.simpleFunctions
@@ -315,7 +316,8 @@ internal class BlockGenerator(private val codegen: CodeGenerator) {
             val isa = codegen.importGlobal(
                     "_NSConcreteStackBlock",
                     llvm.int8PtrType,
-                    CurrentKlibModuleOrigin
+                    CurrentKlibModuleOrigin,
+                    fileOrigin = CompiledKlibFileOrigin.CurrentFile
             )
 
             val flags = llvm.int32((1 shl 25) or (1 shl 30) or (1 shl 31))
@@ -352,7 +354,8 @@ private val ObjCExportCodeGeneratorBase.retainBlock: LlvmCallable
                 "objc_retainBlock",
                 LlvmRetType(llvm.int8PtrType),
                 listOf(LlvmParamType(llvm.int8PtrType)),
-                origin = CurrentKlibModuleOrigin
+                origin = context.standardLlvmSymbolsOrigin,
+                fileOrigin = CompiledKlibFileOrigin.StdlibRuntime
         )
         return llvm.externalFunction(functionProto)
     }

frikke/kotlin

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/Imports.kt
@@ -12,12 +12,17 @@ import org.jetbrains.kotlin.konan.library.KonanLibrary
 import org.jetbrains.kotlin.library.metadata.CompiledKlibModuleOrigin
 import org.jetbrains.kotlin.library.metadata.SyntheticModulesOrigin
 import org.jetbrains.kotlin.library.metadata.klibModuleOrigin
+import org.jetbrains.kotlin.library.metadata.CompiledKlibFileOrigin
+import org.jetbrains.kotlin.library.KotlinLibrary
 import org.jetbrains.kotlin.resolve.descriptorUtil.module
 
 internal interface LlvmImports {
-    fun add(origin: CompiledKlibModuleOrigin, onlyBitcode: Boolean = false)
+    data class LibraryFile(val library: KotlinLibrary, val fqName: String, val filePath: String)
+
+    fun add(origin: CompiledKlibModuleOrigin, fileOrigin: CompiledKlibFileOrigin, onlyBitcode: Boolean = false)
     fun bitcodeIsUsed(library: KonanLibrary): Boolean
     fun nativeDependenciesAreUsed(library: KonanLibrary): Boolean
+    fun usedBitcode(): List<LibraryFile>
 }
 
 internal val DeclarationDescriptor.llvmSymbolOrigin: CompiledKlibModuleOrigin

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/LlvmFunctionPrototype.kt
@@ -12,13 +12,14 @@ import llvm.*
 import org.jetbrains.kotlin.backend.konan.Context
 import org.jetbrains.kotlin.backend.konan.RuntimeNames
 import org.jetbrains.kotlin.backend.konan.ir.llvmSymbolOrigin
+import org.jetbrains.kotlin.library.metadata.CompiledKlibFileOrigin
+import org.jetbrains.kotlin.library.metadata.CompiledKlibModuleOrigin
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.types.isNothing
 import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.util.isSuspend
 import org.jetbrains.kotlin.ir.util.isThrowable
-import org.jetbrains.kotlin.library.metadata.CompiledKlibModuleOrigin
 
 /**
  * Add attributes to LLVM function declaration and its invocation.
@@ -157,6 +158,7 @@ internal class LlvmFunctionProto(
         parameterTypes: List<LlvmParamType> = emptyList(),
         functionAttributes: List<LlvmFunctionAttribute> = emptyList(),
         val origin: CompiledKlibModuleOrigin,
+        val fileOrigin: CompiledKlibFileOrigin,
         isVararg: Boolean = false,
         val independent: Boolean = false,
 ) : LlvmFunctionSignature(returnType, parameterTypes, isVararg, functionAttributes) {
@@ -164,8 +166,9 @@ internal class LlvmFunctionProto(
             name: String,
             signature: LlvmFunctionSignature,
             origin: CompiledKlibModuleOrigin,
+            fileOrigin: CompiledKlibFileOrigin,
             independent: Boolean = false,
-    ) : this(name, signature.returnType, signature.parameterTypes, signature.functionAttributes, origin, signature.isVararg, independent)
+    ) : this(name, signature.returnType, signature.parameterTypes, signature.functionAttributes, origin, fileOrigin, signature.isVararg, independent)
 
     constructor(irFunction: IrFunction, symbolName: String, contextUtils: ContextUtils) : this(
             name = symbolName,
@@ -173,6 +176,7 @@ internal class LlvmFunctionProto(
             parameterTypes = contextUtils.getLlvmFunctionParameterTypes(irFunction),
             functionAttributes = inferFunctionAttributes(contextUtils, irFunction),
             origin = irFunction.llvmSymbolOrigin,
+            fileOrigin = contextUtils.context.irLinker.getFileOrigin(irFunction),
             independent = irFunction.hasAnnotation(RuntimeNames.independent)
     )
 }

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/objcexport/BlockPointerSupport.kt
@@ -11,9 +11,10 @@ import llvm.LLVMStoreSizeOfType
 import llvm.LLVMValueRef
 import org.jetbrains.kotlin.backend.konan.llvm.*
 import org.jetbrains.kotlin.backend.konan.objcexport.BlockPointerBridge
+import org.jetbrains.kotlin.library.metadata.CompiledKlibFileOrigin
+import org.jetbrains.kotlin.library.metadata.CurrentKlibModuleOrigin
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.util.simpleFunctions
-import org.jetbrains.kotlin.library.metadata.CurrentKlibModuleOrigin
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 internal fun ObjCExportCodeGeneratorBase.generateBlockToKotlinFunctionConverter(
@@ -318,7 +319,8 @@ internal class BlockGenerator(private val codegen: CodeGenerator) {
             val isa = codegen.importGlobal(
                     "_NSConcreteStackBlock",
                     llvm.int8PtrType,
-                    CurrentKlibModuleOrigin
+                    CurrentKlibModuleOrigin,
+                    fileOrigin = CompiledKlibFileOrigin.CurrentFile
             )
 
             val flags = llvm.int32((1 shl 25) or (1 shl 30) or (1 shl 31))
@@ -350,12 +352,8 @@ internal class BlockGenerator(private val codegen: CodeGenerator) {
 }
 
 private val ObjCExportCodeGeneratorBase.retainBlock: LlvmCallable
-    get() {
-        val functionProto = LlvmFunctionProto(
-                "objc_retainBlock",
-                LlvmRetType(llvm.int8PtrType),
-                listOf(LlvmParamType(llvm.int8PtrType)),
-                origin = CurrentKlibModuleOrigin
-        )
-        return llvm.externalFunction(functionProto)
-    }
+    get() = llvm.externalStdlibFunction(
+            "objc_retainBlock",
+            LlvmRetType(llvm.int8PtrType),
+            listOf(LlvmParamType(llvm.int8PtrType))
+    )