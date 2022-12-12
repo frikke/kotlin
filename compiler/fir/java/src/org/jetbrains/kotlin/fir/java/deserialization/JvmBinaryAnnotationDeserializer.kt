b584b9fbc9f2cc7f8b043ae41357a90507512dbe
Roman Elizarov <elizarov@gmail.com>
2022-12-12 17:06:26 +0100

    Don't retain ByteArray content in JvmBinaryAnnotationDeserializer

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/deserialization/JvmBinaryAnnotationDeserializer.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/deserialization/JvmBinaryAnnotationDeserializer.kt
@@ -21,25 +21,39 @@ import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.protobuf.MessageLite
+import org.jetbrains.kotlin.resolve.constants.ClassLiteralValue
 import org.jetbrains.kotlin.serialization.deserialization.builtins.BuiltInSerializerProtocol
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
 import org.jetbrains.kotlin.utils.addToStdlib.runIf
+
+
+fun JvmBinaryAnnotationDeserializer(
+    session: FirSession,
+    kotlinBinaryClass: KotlinJvmBinaryClass,
+    kotlinClassFinder: KotlinClassFinder,
+    byteContent: ByteArray?
+) = JvmBinaryAnnotationDeserializer(
+    session,
+    kotlinBinaryClass,
+    kotlinClassFinder,
+    readMemberAnnotationNodes(kotlinBinaryClass, byteContent)
+)
 
 class JvmBinaryAnnotationDeserializer(
     val session: FirSession,
     kotlinBinaryClass: KotlinJvmBinaryClass,
     kotlinClassFinder: KotlinClassFinder,
-    byteContent: ByteArray?
+    memberAnnotationNodes: Map<MemberSignature, List<JvmAnnotationNode>>
 ) : AbstractAnnotationDeserializer(session, BuiltInSerializerProtocol) {
-    private val annotationInfo by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        session.loadMemberAnnotations(kotlinBinaryClass, byteContent, kotlinClassFinder)
+    private val annotationInfo: MemberAnnotations by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        session.loadMemberAnnotations(memberAnnotationNodes, kotlinClassFinder)
     }
 
     private val annotationInfoForDefaultImpls by lazy(LazyThreadSafetyMode.PUBLICATION) {
         val defaultImplsClassId = kotlinBinaryClass.classId.createNestedClassId(Name.identifier(JvmAbi.DEFAULT_IMPLS_CLASS_NAME))
-        val (defaultImplsClass, defaultImplsByteContent) = kotlinClassFinder.findKotlinClassOrContent(defaultImplsClassId) as? KotlinClassFinder.Result.KotlinClass
+        val (_, defaultImplsByteContent) = kotlinClassFinder.findKotlinClassOrContent(defaultImplsClassId) as? KotlinClassFinder.Result.KotlinClass
             ?: return@lazy null
-        session.loadMemberAnnotations(defaultImplsClass, defaultImplsByteContent, kotlinClassFinder)
+        session.loadMemberAnnotations(readMemberAnnotationNodes(kotlinBinaryClass, defaultImplsByteContent), kotlinClassFinder)
     }
 
     override fun inheritAnnotationInfo(parent: AbstractAnnotationDeserializer) {
@@ -269,12 +283,25 @@ private data class MemberAnnotations(val memberAnnotations: MutableMap<MemberSig
 
 // TODO: better to be in KotlinDeserializedJvmSymbolsProvider?
 private fun FirSession.loadMemberAnnotations(
-    kotlinBinaryClass: KotlinJvmBinaryClass,
-    byteContent: ByteArray?,
+    memberAnnotationNodes: Map<MemberSignature, List<JvmAnnotationNode>>,
     kotlinClassFinder: KotlinClassFinder,
 ): MemberAnnotations {
-    val memberAnnotations = hashMapOf<MemberSignature, MutableList<FirAnnotation>>()
     val annotationsLoader = AnnotationsLoader(this, kotlinClassFinder)
+    val memberAnnotations = memberAnnotationNodes.mapValuesTo(HashMap()) { entry ->
+        val result = mutableListOf<FirAnnotation>()
+        entry.value.map {
+            it.accept(annotationsLoader.loadAnnotationIfNotSpecial(it.classId, result))
+        }
+        result
+    }
+    return MemberAnnotations(memberAnnotations)
+}
+
+private fun readMemberAnnotationNodes(
+    kotlinBinaryClass: KotlinJvmBinaryClass,
+    byteContent: ByteArray?,
+): Map<MemberSignature, List<JvmAnnotationNode>> {
+    val memberAnnotations = HashMap<MemberSignature, MutableList<JvmAnnotationNode>>()
 
     kotlinBinaryClass.visitMembers(object : KotlinJvmBinaryClass.MemberVisitor {
         override fun visitMethod(name: Name, desc: String): KotlinJvmBinaryClass.MethodAnnotationVisitor {
@@ -296,14 +323,11 @@ private fun FirSession.loadMemberAnnotations(
                 index: Int,
                 classId: ClassId,
                 source: SourceElement
-            ): KotlinJvmBinaryClass.AnnotationArgumentVisitor? {
-                val paramSignature = MemberSignature.fromMethodSignatureAndParameterIndex(signature, index)
-                var result = memberAnnotations[paramSignature]
-                if (result == null) {
-                    result = arrayListOf()
-                    memberAnnotations[paramSignature] = result
+            ): KotlinJvmBinaryClass.AnnotationArgumentVisitor {
+                val paramSignature = MemberSignature.fromMethodSignatureAndParameterIndex(this.signature, index)
+                return JvmAnnotationNode(classId).also { node ->
+                    memberAnnotations.getOrPut(paramSignature) { ArrayList() }.add(node)
                 }
-                return annotationsLoader.loadAnnotationIfNotSpecial(classId, result)
             }
 
             override fun visitAnnotationMemberDefaultValue(): KotlinJvmBinaryClass.AnnotationArgumentVisitor? {
@@ -313,10 +337,10 @@ private fun FirSession.loadMemberAnnotations(
         }
 
         open inner class MemberAnnotationVisitor(protected val signature: MemberSignature) : KotlinJvmBinaryClass.AnnotationVisitor {
-            private val result = arrayListOf<FirAnnotation>()
+            private val result = ArrayList<JvmAnnotationNode>()
 
             override fun visitAnnotation(classId: ClassId, source: SourceElement): KotlinJvmBinaryClass.AnnotationArgumentVisitor? {
-                return annotationsLoader.loadAnnotationIfNotSpecial(classId, result)
+                return JvmAnnotationNode(classId).also { result += it }
             }
 
             override fun visitEnd() {
@@ -327,5 +351,111 @@ private fun FirSession.loadMemberAnnotations(
         }
     }, byteContent)
 
-    return MemberAnnotations(memberAnnotations)
+    return memberAnnotations
+}
+
+private enum class Op { Visit, VisitClassLiteral, VisitEnum, VisitAnnotation, VisitArray }
+
+class JvmAnnotationNode(val classId: ClassId) : KotlinJvmBinaryClass.AnnotationArgumentVisitor {
+    private val a = ArrayList<Any?>()
+
+    override fun visit(name: Name?, value: Any?) {
+        a += Op.Visit
+        a += name
+        a += value
+    }
+
+    override fun visitClassLiteral(name: Name?, value: ClassLiteralValue) {
+        a += Op.VisitClassLiteral
+        a += name
+        a += value
+    }
+
+    override fun visitEnum(name: Name?, enumClassId: ClassId, enumEntryName: Name) {
+        a += Op.VisitEnum
+        a += name
+        a += enumClassId
+        a += enumEntryName
+    }
+
+    override fun visitAnnotation(name: Name?, classId: ClassId): KotlinJvmBinaryClass.AnnotationArgumentVisitor {
+        a += Op.VisitAnnotation
+        a += name
+        return JvmAnnotationNode(classId).also { a += it }
+    }
+
+    override fun visitArray(name: Name?): KotlinJvmBinaryClass.AnnotationArrayArgumentVisitor {
+        a += Op.VisitArray
+        a += name
+        return JvmAnnotationArrayArgumentNode().also { a += it }
+    }
+
+    override fun visitEnd() {}
+
+    fun accept(visitor: KotlinJvmBinaryClass.AnnotationArgumentVisitor?) {
+        if (visitor == null) return
+        var i = 0
+        while (i < a.size) {
+            when (a[i++]) {
+                Op.Visit -> visitor.visit(a[i++] as Name?, a[i++])
+                Op.VisitClassLiteral -> visitor.visitClassLiteral(a[i++] as Name?, a[i++] as ClassLiteralValue)
+                Op.VisitEnum -> visitor.visitEnum(a[i++] as Name?, a[i++] as ClassId, a[i++] as Name)
+                Op.VisitAnnotation -> {
+                    val name = a[i++] as Name?
+                    val node = a[i++] as JvmAnnotationNode
+                    node.accept(visitor.visitAnnotation(name, node.classId))
+                }
+                Op.VisitArray -> {
+                    val name = a[i++] as Name?
+                    val node = a[i++] as JvmAnnotationArrayArgumentNode
+                    node.accept(visitor.visitArray(name))
+                }
+            }
+        }
+        visitor.visitEnd()
+    }
+}
+
+class JvmAnnotationArrayArgumentNode : KotlinJvmBinaryClass.AnnotationArrayArgumentVisitor {
+    private val a = ArrayList<Any?>()
+
+    override fun visit(value: Any?) {
+        a += Op.Visit
+        a += value
+    }
+
+    override fun visitEnum(enumClassId: ClassId, enumEntryName: Name) {
+        a += Op.VisitEnum
+        a += enumClassId
+        a += enumEntryName
+    }
+
+    override fun visitClassLiteral(value: ClassLiteralValue) {
+        a += Op.VisitClassLiteral
+        a += value
+    }
+
+    override fun visitAnnotation(classId: ClassId): KotlinJvmBinaryClass.AnnotationArgumentVisitor {
+        a += Op.VisitAnnotation
+        return JvmAnnotationNode(classId).also { a += it }
+    }
+
+    override fun visitEnd() {}
+
+    fun accept(visitor: KotlinJvmBinaryClass.AnnotationArrayArgumentVisitor?) {
+        if (visitor == null) return
+        var i = 0
+        while (i < a.size) {
+            when (a[i++]) {
+                Op.Visit -> visitor.visit(a[i++])
+                Op.VisitClassLiteral -> visitor.visitClassLiteral(a[i++] as ClassLiteralValue)
+                Op.VisitEnum -> visitor.visitEnum(a[i++] as ClassId, a[i++] as Name)
+                Op.VisitAnnotation -> {
+                    val node = a[i++] as JvmAnnotationNode
+                    node.accept(visitor.visitAnnotation(node.classId))
+                }
+            }
+        }
+        visitor.visitEnd()
+    }
 }
